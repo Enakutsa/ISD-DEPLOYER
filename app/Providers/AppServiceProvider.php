5d22ecc75da8a44aeb3bcbd98b1bf101d78602ce
enakutsa <enakutsakokouespoir@gmail.com>
2026-02-23 08:46:25 +0000

    ajout public/build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Relations\Relation;
-
 class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        \URL::forceScheme('https');
+        
         // Configuration du morph map pour les relations polymorphiques
         Relation::enforceMorphMap([
             'utilisateur' => 'App\Models\Utilisateur',
@@ -17,8 +16,6 @@ class AppServiceProvider extends ServiceProvider
             'commande' => 'App\Models\Commande',
             'blog' => 'App\Models\Blog',
             'user' => 'App\Models\User',
-            
-            // Ajoutez d'autres modèles si nécessaire
         ]);
     }
 }
